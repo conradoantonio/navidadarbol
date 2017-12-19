93b5036a92fc431347f1ec7e5a78927cb2bf8f75
conradoantonio <anton_con@hotmail.com>
2017-12-18 18:05:46 -0600

    Se cambió el texto que aparece en la etiqueta title

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>@yield('title', isset($title) ? $title .' | Papá noel' : 'Papá noel')</title>
+    <title>@yield('title', isset($title) ? $title .' | Papá Noel' : 'Papá Noel')</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
